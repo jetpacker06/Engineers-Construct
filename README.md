d29a79c5699df5bf178abb150eb69118a335d18d
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-11-07 22:17:20 -0600

    Set up workspace

---
+++ README.md
@@ -0,0 +1,5 @@
+# Engineer's Construct
+### By jetpacker06
+A bridge between Tinker's Construct and Create!
+Lots of content planned, such as recipes, items, new ways to obtain items, and maybe a few materials.   
+Started 11/6/2022.
--- src/main/java/com/jetpacker06/econstruct/EngineersConstruct.java
+++ src/main/java/com/jetpacker06/econstruct/EngineersConstruct.java
@@ -7,22 +7,15 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-//Mod ID can have ONLY LOWERCASE LETTERS. No numbers, no underscores, no characters, no capitals, no spaces.
-@Mod("yourmodidhere")
+@Mod("econstruct")
 public class EngineersConstruct {
-    public static final String MOD_ID = "yourmodidhere";
+    public static final String MOD_ID = "econstruct";
     public static final Logger LOGGER = LogManager.getLogger();
 
     public EngineersConstruct() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModBlocks.register(eventBus);
         ModItems.register(eventBus);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-
     }
 }
